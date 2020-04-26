d60668a5ae94918021a9b3df20777b9a9083f237
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-26 10:47:47 +0100

    Created styles for the boxes

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet} from 'react-native';
 import {Card, Button, Icon} from 'react-native-elements';
 
 const URL = 'http://localhost:3000/api/v1';
@@ -22,20 +22,25 @@ const Home = () => {
 
   return (
     <Card title="HELLO WORLD" image={require('../images/pic2.jpg')}>
-      <Text style={{marginBottom: 10}}>
+      <Text style={styles.text}>
         The idea with React Native Elements is more about component structure
         than actual design.
       </Text>
       <Button
         icon={<Icon name="code" color="#ffffff" />}
-        buttonStyle={{
-          borderRadius: 0,
-          marginLeft: 0,
-          marginRight: 0,
-          marginBottom: 0,
-        }}
+        buttonStyle={styles.button}
         title="VIEW NOW"
       />
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {marginBottom: 10},
+  button: {
+    borderRadius: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+  },
+});
