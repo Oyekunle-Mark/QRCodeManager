dc5c4be3c2b67ca5d415e0a5c999129777d26897
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-26 10:36:49 +0100

    Setting up a callback for qr codes

---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect, useCallback} from 'react react';
+
+const URL = 'http://localhost:3000/api/v1';
+
+const Home = () => {
+  const [qRCodes, setQRCodes] = useState([]);
+
+  const getQRCodes = useCallback(async () => {
+    const response = await fetch(URL);
+    const qrs = await response.json();
+
+    if (response.ok) {
+      setQRCodes(qrs.data);
+    }
+  }, []);
+};
